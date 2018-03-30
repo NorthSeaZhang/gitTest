adee499776fe0273ca73f8de5b51bca0b85822ef
SeaCample <zhang20ke@163.com>
2018-03-30 11:32:37 +0800

    add file

---
+++ Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  GitTest
+//
+//  Created by SeaCample on 2018/3/30.
+//  Copyright © 2018年 0. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by SeaCample on 17/5/23.
+//  Copyright © 2017年 0. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 这是个测试
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)say{
+    NSLog(@"say is ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
